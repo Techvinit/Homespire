9e56fc4ded53ab75992bbc53ca603e9d55feeb88
vint <vinitgarole@34gmail.com>
2024-08-07 23:07:02 +0530

    code commented

--- demo/main.py
+++ demo/main.py
@@ -2,7 +2,7 @@ import openpyxl
 
 data_file= openpyxl.load_workbook("Book1.xlsx")
 raw_data= data_file["Sheet1"]
-print(raw_data.max_row)
+#print(raw_data.max_row)....
 
 for product_row in range(2, raw_data.max_row + 1):
 
